9b546323720124f747330adf41303ff5a7f5b2bb
micaela1001 <micaela1001.sh@gmail.com>
2023-01-30 15:49:34 +0900

    question_roop

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var question = prompt("日本の首都は？");
-
-if (question === 東京) {
-  alert("正解です！");
-} else {
+while (question !== "東京") {
   alert("不正解です！");
+  prompt("日本の首都は？");
+  if (question !== "東京") break;
 }
+alert("正解です！");
